ecbe2769560b760448cc5bbebe1782b01d32ea03
Gowdaraju <57538893+Gowdaraju@users.noreply.github.com>
2020-12-17 16:06:07 -0500

    h1 font changed

--- src/List.js
+++ src/List.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const List = ({ people }) => {
+  console.log(people);
   return (
     <>
       {people.map((person) => {
         const { id, name, age, image } = person;
+
         return (
           <article key={id} className="person">
             <img src={image} alt={name} />
